f7d9d27c46115db729ea5770d197e169bf31154e
Breno Soares <brenosoares4303@gmail.com>
2024-11-09 04:45:20 +0000

    window!

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 BlockForge
 Craftorium
 TerraFabric
+
+# Dependencies
+GLFW - (Window creation)
+GLAD - (OpenGL)
+ImGui - (Editor interface)
+spdlog - (Logging)
+entt - (ECS)
+
+# Portability
+Windows and Linux
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <math.h>
 
 int main()
 {
-    std::cout << "Hello Craftorium" << std::endl;
-    
-    return 0;
+    GLFWwindow* window;
+
+    glfwInit();
+
+    window = glfwCreateWindow(800, 600, "TerraFabric", 0, 0);
+
+    glfwMakeContextCurrent(window);
+
+    // main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, sin(glfwGetTime()), 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render here
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 }
